7e73e6e9c94a201c49104e069c7d0f9bddc44467
RimidalU <stankevichuuladimiru@gmail.com>
2023-04-27 22:34:52 +0300

    feat: add create table commands

---
+++ var/mydb/departments_store.sql
@@ -0,0 +1,29 @@
+CREATE TABLE
+    IF NOT EXISTS cscart_departments (
+        department_id INT(11) unsigned NOT NULL auto_increment,
+        position INT(11) unsigned NOT NULL default '0',
+        status varchar(1) NOT NULL default 'A',
+        timestamp INT(11) unsigned NOT NULL default '0',
+        upd_timestamp INT(11) unsigned NOT NULL default '0',
+        chief_id INT(11) unsigned NOT NULL default '0',
+        KEY (chief_id),
+        PRIMARY KEY (department_id)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb3;
+
+CREATE TABLE
+    IF NOT EXISTS cscart_department_descriptions (
+        department_id INT(11) unsigned NOT NULL default '0',
+        lang_code char(2) NOT NULL default '',
+        department varchar(255) NOT NULL default '',
+        description text null,
+        PRIMARY KEY (department_id, lang_code)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb3;
+
+CREATE TABLE
+    IF NOT EXISTS cscart_department_links (
+        department_id INT(11) unsigned NOT NULL default '0',
+        user_id INT(11) unsigned NOT NULL default '0',
+        KEY (user_id),
+        KEY (department_id),
+        PRIMARY KEY (department_id, user_id)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb3;
